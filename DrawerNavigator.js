5dadcd442178fe5c7a3d0272b333f1f5edbb5551
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-09-26 18:30:52 -0300

    Add files via upload

---
+++ DrawerNavigator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import  TabNavigator  from "./TabNavigator";
+import ProfileScreen from "../screens/Profile";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tela Inicial" component={TabNavigator}/>
+            <Drawer.Screen name="Perfil" component={ProfileScreen}/>
+        </Drawer.Navigator>
+    );
+};
+
+export default DrawerNavigator;
